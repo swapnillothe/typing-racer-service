83ccdb4c00541bbcd2d680adcf5fc2e77b6fd9f4
Swapnil Lothe <swapnillothe@Swapnils-MacBook-Pro.local>
2020-03-18 03:08:42 +0530

    Solve cors issue

--- src/typing_racer_service/handler.clj
+++ src/typing_racer_service/handler.clj
@@ -7,10 +7,25 @@
 (def para (atom (gen/paragraph {:lang           :en
                                 :sentence-range [1 5]})))
 
+(def default-headers {
+                      "Content-Type"                 "text/plain"
+                      "Access-Control-Allow-Origin"  "*"
+                      "Access-Control-Allow-Methods" "GET"
+                      "Access-Control-Allow-Headers" "X-Requested-With,Content-Type,Cache-Control,Origin,Accept"
+                      })
+
+(defn wrap-cors
+  ([body] (wrap-cors body nil nil))
+  ([body headers] (wrap-cors body headers nil))
+  ([body headers response]
+   (merge {:status  200
+           :headers (merge default-headers headers)
+           :body    body} response)))
+
 (defroutes app-routes
-           (GET "/" [] "Hello World")
-           (GET "/paragraph" [] para)
-           (route/not-found "Not Found"))
+           (GET "/" [] (wrap-cors "Hello World"))
+           (GET "/paragraph" [] (wrap-cors @para))
+           (route/not-found (wrap-cors "Not Found")))
 
 
 (defn main []
