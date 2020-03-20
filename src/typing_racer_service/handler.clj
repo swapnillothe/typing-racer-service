61d517d0f300d989da5b2b3df7c72031ad10a603
affishaikh <sk.aftab1999@gmail.com>
2020-03-20 11:44:48 +0530

    Calculate speed based on the typed words

--- src/typing_racer_service/handler.clj
+++ src/typing_racer_service/handler.clj
@@ -2,7 +2,6 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [faker.generate :as gen]
-            [clojure.string :as str]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (def para (atom (gen/sentence {:lang :en})))
@@ -13,8 +12,8 @@
   (float (/ ms 60000)))
 
 (defn calculate-speed
-  [st words et]
-  (Math/round (/ (count (str/split words #" ")) (convert-to-minutes (- et st)))))
+  [st words-count et]
+  (Math/round (/ words-count (convert-to-minutes (- et st)))))
 
 (defn wrap-cors
   [handler]
@@ -24,14 +23,15 @@
 (defn start-race []
   (reset! start-time (.getTime (java.util.Date.))))
 
-(defn end-race []
-  (calculate-speed @start-time @para (.getTime (java.util.Date.))))
+(defn end-race [words-count]
+  (calculate-speed @start-time words-count (.getTime (java.util.Date.))))
 
 (defroutes app-routes
            (GET "/" [] "Hello World")
            (GET "/paragraph" [] @para)
            (POST "/start-race" [] (str (start-race)))
-           (POST "/end-race" [] (str (end-race) " WPM"))
+           (POST "/end-race" {:keys [params]}
+             (str (end-race (read-string (:words-count params))) " WPM"))
            (route/not-found "Not Found"))
 
 
