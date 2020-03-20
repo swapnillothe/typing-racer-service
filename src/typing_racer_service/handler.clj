971442fa1be4f25065b24a2162fbf2fef2810385
affishaikh <sk.aftab1999@gmail.com>
2020-03-20 23:32:22 +0530

    Create api for join-race

--- src/typing_racer_service/handler.clj
+++ src/typing_racer_service/handler.clj
@@ -4,6 +4,11 @@
             [faker.generate :as gen]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def races (atom {1234
+                  {:no-of-players 2
+                   :paragraph     (gen/sentence {:lang :en})
+                   :players       []}}))
+
 (def para (atom (gen/sentence {:lang :en})))
 (def start-time (atom nil))
 
@@ -26,12 +31,19 @@
 (defn end-race [words-count]
   (calculate-speed @start-time words-count (.getTime (java.util.Date.))))
 
+(defn join-race
+  [player]
+  (swap! races
+         #(update-in % [(read-string (:race-id player)) :players]
+                     (fn [players] (conj players (merge player {:speed 0}))))))
+
 (defroutes app-routes
-           (GET "/" [] "Hello World")
            (GET "/paragraph" [] @para)
            (POST "/start-race" [] (str (start-race)))
            (POST "/end-race" {:keys [params]}
              (str (end-race (read-string (:words-count params))) " WPM"))
+           (POST "/join-race" {:keys [params]}
+             (join-race params) {:status 200})
            (route/not-found "Not Found"))
 
 
